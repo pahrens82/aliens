3b2ea75f6c791cbf760b53c8f205232d967ad808
Patrick Ahrens <22923430+pahrens82@users.noreply.github.com>
2025-11-30 22:07:50 -0500

    Updates the vite config.

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 
 export default defineConfig({
-  plugins: [solid()],
+    plugins: [solid()],
+    homepage: "/aliens/#",
+    base: "./",
+    version: "1.0.0",
 })
